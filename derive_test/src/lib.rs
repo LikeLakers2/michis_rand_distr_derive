b03e67a7df25fcb7c2465b31bfa49e9e198f9faf
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2024-11-08 05:53:23 -0500

    Change the derive macro name from `StandardDistr` to `Standard`

--- derive_test/src/lib.rs
+++ derive_test/src/lib.rs
@@ -1,11 +1,14 @@
-#[derive(michis_rand_distr_derive::StandardDistr)]
+use michis_rand_distr_derive::Standard;
+use rand::distributions::Standard;
+
+#[derive(Standard)]
 pub struct AStructWithBareTypes {
 	pub x: f32,
 	pub y: f32,
 	pub z: f32,
 }
 
-#[derive(michis_rand_distr_derive::StandardDistr)]
+#[derive(Standard)]
 pub struct AStructWithTypeParams<F> {
 	pub inner: F,
 	pub other_inner: f32,
--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@ use syn::parse_macro_input;
 mod standard;
 mod uniform;
 
-#[proc_macro_derive(StandardDistr)]
+#[proc_macro_derive(Standard)]
 pub fn derive_standard_distribution(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 	let input = parse_macro_input!(input);
 	self::standard::derive(input).into()
 }
 
-#[proc_macro_derive(UniformDistr)]
+#[proc_macro_derive(Uniform)]
 pub fn derive_uniform_distribution(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 	let input = parse_macro_input!(input);
 	self::uniform::derive(input).into()
