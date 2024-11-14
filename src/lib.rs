b697c501743b3bd2d6d7238ad74ee96b6135ad14
MichiRecRoom <1008889+LikeLakers2@users.noreply.github.com>
2024-11-14 02:40:55 -0500

    format with rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -7,27 +7,22 @@ mod standard_distribution;
 mod uniform_sampler;
 
 #[proc_macro_derive(StandardDistribution)]
-pub fn derive_standard_distribution(
-	input_item: TokenStream1,
-) -> TokenStream1 {
+pub fn derive_standard_distribution(input_item: TokenStream1) -> TokenStream1 {
 	let derive_input = parse_macro_input!(input_item);
-	let derive_opts = match self::standard_distribution::DeriveData::from_derive_input(&derive_input) {
-		Ok(v) => v,
-		Err(e) => return e.write_errors().into(),
-	};
+	let derive_opts =
+		match self::standard_distribution::DeriveData::from_derive_input(&derive_input) {
+			Ok(v) => v,
+			Err(e) => return e.write_errors().into(),
+		};
 	derive_opts.do_derive().into()
 }
 
 #[proc_macro_derive(SampleUniform)]
-pub fn derive_sample_uniform(
-	_input_item: TokenStream1,
-) -> TokenStream1 {
+pub fn derive_sample_uniform(_input_item: TokenStream1) -> TokenStream1 {
 	todo!()
 }
 
 #[proc_macro_derive(UniformSampler)]
-pub fn derive_uniform_sampler(
-	_input_item: TokenStream1,
-) -> TokenStream1 {
+pub fn derive_uniform_sampler(_input_item: TokenStream1) -> TokenStream1 {
 	todo!()
 }
